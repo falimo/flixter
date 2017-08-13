63eb40d2298bff55fd354b899c712071271b4999
Farouk Alimohamed <falimo@yahoo.com>
2017-08-13 10:31:23 +0000

    L28 -challenge - only logged-in users can view lesson videos

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
 
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "Yo, please enroll in the course to see the lessons"
+
+      # render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
 
   helper_method :current_lesson
   def current_lesson
